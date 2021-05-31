5cf45c71adc5d4f0c9c32813ff1c71199c44793b
Federico Verstraeten <federico.verstraeten@gmail.com>
2021-05-31 13:50:20 -0300

    changing indentation 4 to 2 spaces

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ licelcontroller.settings.HOST = "10.49.234.234"
 licelcontroller.settings.PORT = 2055
 
 if __name__ == '__main__':
-    c = Controller()
-##    c.select(0,1,2,3)
-    c.select(1)
-    c.mstart()
-##    c.mstop()
-    c.run()
-    time.sleep(1)
-    c.mstop()
-    c.data()
-    c.run()
+  c = Controller()
+##  c.select(0,1,2,3)
+  c.select(1)
+  c.mstart()
+##  c.mstop()
+  c.run()
+  time.sleep(1)
+  c.mstop()
+  c.data()
+  c.run()
--- licelcontroller/interface.py
+++ licelcontroller/interface.py
@@ -6,69 +6,69 @@ from licelcontroller import settings, tr_commands
 
 
 class Controller():
-    def __init__(self, *args, **kwargs):
-        self.commands_to_execute = []
+  def __init__(self, *args, **kwargs):
+    self.commands_to_execute = []
 
-    def run(self):
-        with Pool(1) as pool:
-            try:
-                results = pool.map(client, self.commands_to_execute)
-                print(results)
-            except OSError:
-                print("Connection failed..")
-                return
-            return results
+  def run(self):
+    with Pool(1) as pool:
+      try:
+        results = pool.map(client, self.commands_to_execute)
+        print(results)
+      except OSError:
+        print("Connection failed..")
+        return
+      return results
 
-    def crlf(self):
-        return "\r\n"
+  def crlf(self):
+    return "\r\n"
 
-    def to_bytes(self, command):
-        return command.encode()
+  def to_bytes(self, command):
+    return command.encode()
 
-    def add(self, command):
-        self.commands_to_execute.append(
-            self.to_bytes(command)
-        )
+  def add(self, command):
+    self.commands_to_execute.append(
+      self.to_bytes(command)
+    )
 
-    def select(self, *devices):
-        transient_recorders = ', '.join(str(device) for device in devices)
-        command = tr_commands.SELECT + " " + transient_recorders + self.crlf()
-        self.add(command)
+  def select(self, *devices):
+    transient_recorders = ', '.join(str(device) for device in devices)
+    command = tr_commands.SELECT + " " + transient_recorders + self.crlf()
+    self.add(command)
 
-    def pmtgain(self, device, voltage):
-        command = tr_commands.PMTGAIN + " " + str(device) \
-                  + " " + str(voltage) + self.crlf()
-        self.add(command)
+  def pmtgain(self, device, voltage):
+    command = tr_commands.PMTGAIN + " " + str(device) \
+          + " " + str(voltage) + self.crlf()
+    self.add(command)
 
-    def mstart(self):
-        command = tr_commands.MSTART + self.crlf()
-        self.add(command)
+  def mstart(self):
+    command = tr_commands.MSTART + self.crlf()
+    self.add(command)
 
-    def mstop(self):
-        command = tr_commands.MSTOP + self.crlf()
-        self.add(command)
-    
-    def data(self):
-        command = 'DATA? 1 8000 LSW A' + self.crlf()
-        self.add(command)
+  def mstop(self):
+    command = tr_commands.MSTOP + self.crlf()
+    self.add(command)
+  
+  def data(self):
+    command = 'DATA? 1 8000 LSW A' + self.crlf()
+    self.add(command)
 
 def client(command):
 
-    @asyncio.coroutine
-    def tcp_echo_client(loop):
-        reader, writer = yield from asyncio.open_connection(
-            settings.HOST, settings.PORT, loop=loop)
-        print(f'Send: {command.decode()!r}')
-        writer.write(command)
-        data = yield from reader.readline()
-        print("Client received {!r} from server".format(data))
-        writer.close()
-        return data.decode()
+  @asyncio.coroutine
+  def tcp_echo_client(loop):
+    reader, writer = yield from asyncio.open_connection(
+      settings.HOST, settings.PORT, loop=loop)
+    print(f'Send: {command.decode()!r}')
+    writer.write(command)
+    data = yield from reader.readline()
+    print("Client received {!r} from server".format(data))
+    writer.close()
+    return data.decode()
 
-    loop = asyncio.get_event_loop()
-    data = loop.run_until_complete(
-        asyncio.gather(
-            tcp_echo_client(loop)
-        )
+  loop = asyncio.get_event_loop()
+  data = loop.run_until_complete(
+    asyncio.gather(
+      tcp_echo_client(loop)
     )
-    return data[0].replace('\r\n', '')
+  )
+  return data[0].replace('\r\n', '')
