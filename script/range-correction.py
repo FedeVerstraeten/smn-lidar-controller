0c616a0bf3871561bf546b98e6d1a87fe967300d
Federico Verstraeten <federico.verstraeten@gmail.com>
2021-08-31 18:56:44 -0300

    new script update

--- script/range-correction.py
+++ script/range-correction.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-LIDAR_FILE='analog.txt'
+LIDAR_FILE='/home/fverstra/Desktop/analog_dia_20mV.txt'
 
 data_csv = pd.read_csv(LIDAR_FILE,sep='\n',header=None)
 data=pd.DataFrame(data_csv)
@@ -13,5 +13,13 @@ x = np.arange(0, len(y), 1)
 # LiDAR signal range correction since bin number 1000
 
 yr = (y-np.mean(y[1000:]))*(x**2)
-plt.plot(x,yr)
+fig,(ax1,ax2)=plt.subplots(2)
+
+fig.suptitle('LiDAR SMN')
+ax1.set(xlabel='bins', ylabel='voltage (mV)')
+ax2.set(xlabel='bins', ylabel='mV x m^2')
+ax1.grid()
+ax2.grid()
+ax1.plot(x,y)
+ax2.plot(x,yr)
 plt.show()
